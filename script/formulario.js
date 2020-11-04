0bd865bf5517c37ee425d503a699f29ae325c2cf
Alejandro Limiñana <ale.limi95@gmail.com>
2020-11-04 14:19:50 +0000

    new changes

---
+++ script/formulario.js
@@ -0,0 +1,48 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
+$(document).ready(function() {
+    var current = 1,
+        current_step, next_step, steps;
+    steps = $("fieldset").length;
+    $(".next").click(function() {
+        current_step = $(this).parent();
+        next_step = $(this).parent().next();
+        next_step.show();
+        current_step.hide();
+        setProgressBar(++current);
+    });
+    $(".previous").click(function() {
+        current_step = $(this).parent();
+        next_step = $(this).parent().prev();
+        next_step.show();
+        current_step.hide();
+        setProgressBar(--current);
+    });
+    setProgressBar(current);
+
+    // Change progress bar action
+    function setProgressBar(curStep) {
+        var percent = parseFloat(100 / steps) * curStep;
+        percent = percent.toFixed();
+        $(".progress-bar")
+            .css("width", percent + "%")
+            .html(percent + "%");
+    }
+});
